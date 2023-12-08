f99766c8f87cb8c78f3389c26046a14be265916d
Aditya K <adityakaklij11@gmail.com>
2023-12-09 00:27:21 +0530

    Push integration in smart contract

--- Smart Contract/ESLP.sol
+++ Smart Contract/ESLP.sol
@@ -5,6 +5,12 @@ pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
+
+// PUSH Comm Contract Interface
+interface IPUSHCommInterface {
+    function sendNotification(address _channel, address _recipient, bytes calldata _identity) external;
+}
+
 contract ESL {
 
     struct Order {
@@ -55,6 +61,31 @@ contract ESL {
         
         OpenBuyOrders.push(Order (msg.sender, _tokenA, _tokenB, _amount , _price)); // (1 USD = 100, 0.9 USD = 90; The price will be between 0-100)
         isBuyOrderLive[msg.sender] = true;
+
+        // Push Integration 
+
+        IPUSHCommInterface(0x0C34d54a09CFe75BCcd878A469206Ae77E0fe6e7).sendNotification(
+        0x76Ae1122e702f22827cd3246370f796B666634DB, // from channel - recommended to set channel via dApp and put it's value -> then once contract is deployed, go back and add the contract address as delegate for your channel
+        address(this),
+        bytes(
+            string(
+                
+                abi.encodePacked(
+                    "Buy Order Generated",
+                    "+", // segregator
+                    "1", // 
+                    "+", // segregator
+                    "Order at price", // this is notification title
+                    // uint2str(_price),
+                    "Token",
+                    addressToString(_tokenA),
+                    "+", // segregator
+                    "Stabel Coin" // notification body
+                    )
+                )
+            )
+        );
+
         emit BuyOrderCreated(msg.sender, _tokenA, _tokenB, _amount, _price);
 
     }
@@ -67,6 +98,31 @@ contract ESL {
         
         OpenSellOrders.push(Order (msg.sender, _tokenA, _tokenB, _amount, _price));
         isSellOrderLive[msg.sender] = true;
+
+        // Push Integration 
+
+        IPUSHCommInterface(0x0C34d54a09CFe75BCcd878A469206Ae77E0fe6e7).sendNotification(
+        0x76Ae1122e702f22827cd3246370f796B666634DB, // from channel - recommended to set channel via dApp and put it's value -> then once contract is deployed, go back and add the contract address as delegate for your channel
+        address(this),
+        bytes(
+            string(
+                
+                abi.encodePacked(
+                    "Sell Order Generated",
+                    "+", // segregator
+                    "1", // 
+                    "+", // segregator
+                    "Order at price", // this is notification title
+                    // uint2str(_price),
+                    "Token",
+                    addressToString(_tokenA),
+                    "+", // segregator
+                    "Stabel Coin" // notification body
+                    )
+                )
+            )
+        );
+
         emit SellOrderCreated(msg.sender, _tokenA, _tokenB, _amount, _price);
     }
 
@@ -143,5 +199,21 @@ contract ESL {
     function sellOrderCount() public view returns(uint){
         return OpenSellOrders.length;
     }    
+
+     // Helper function to convert address to string
+    function addressToString(address _address) internal pure returns(string memory) {
+        bytes32 _bytes = bytes32(uint256(uint160(_address)));
+        bytes memory HEX = "0123456789abcdef";
+        bytes memory _string = new bytes(42);
+        _string[0] = '0';
+        _string[1] = 'x';
+        for(uint i = 0; i < 20; i++) {
+            _string[2+i*2] = HEX[uint8(_bytes[i + 12] >> 4)];
+            _string[3+i*2] = HEX[uint8(_bytes[i + 12] & 0x0f)];
+        }
+        return string(_string);
+    }
+
+
 }
 
